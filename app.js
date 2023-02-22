b5abf9fe84722afd000e7c37f5a839f7645931c0
KillianDB <killiandomingues57@gmail.com>
2023-02-22 11:57:26 -0300

    add: app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+//Importando path, express e router
+
+const pathe = require('path');
+
+const express = require('express');
+
+const router = require('router');
+
+//Executando express e habilitando ejs
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+//Definindo pasta public como padrão para arquivos estaticos
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Definindo roteador a ser usado
+
+app.use(router);
+
+//Adicionando porta do servidor
+
+app.listen(3000, ()=>{
+    console.log('Servidor rodando');
+});
